8d0a5199586e7010b0a316e2f58b8fd296cf48ef
Raj <mandavkarraj7@gmail.com>
2025-07-22 17:03:05 +0530

    changes in Service

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Services } from '../../services';
 
 
 @Component({
@@ -19,19 +20,21 @@ export class Login {
       "pass": "",
     };
 
- 
+
 
   // constructor(public http: HttpClient) {}
 
   // http = inject(HttpClient);
 
-  http = inject(HttpClient)
+  http = inject(HttpClient);
+  private service = inject(Services);
 
   onclick() {
     this.http.post("http://localhost:3000/users", this.loginData).subscribe((res: any) => void {
       // console.log(res);
     })
   };
+
 
 
 }
--- src/app/services.ts
+++ src/app/services.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class Services {
-  // url: http://localhost:3000/users;
+  http =inject(HttpClient)
+  url= "localhost:3000/users";
+
+  getData(){
+    this.http.get(this.url);
+  }
+  
+  
 }
